3483e829dcbe99a43b1c34996425b3a39aee6931
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-06-19 19:45:22 +0700

    Add dummy data to backend

--- case-3/backend/index.ts
+++ case-3/backend/index.ts
@@ -6,8 +6,15 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/api/products', (req: Request, res: Response) => {
+  var products = require("../data/products.json");
+  res.json(products);
 });
 
 app.listen(port, () => {
